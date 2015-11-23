0c955726595c64c58c233060b92336adbe4db02b
diogoraposo <diogo.mm.raposo@gmail.com>
2015-11-23 09:33:11 +0000

    new things

--- src/braindeadanalyzer/Analyzer.java
+++ src/braindeadanalyzer/Analyzer.java
@@ -1,10 +1,29 @@
 package braindeadanalyzer;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.TreeMap;
+
 public class Analyzer {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		File patternsfile = new File(System.getProperty("user.dir") + "/src/braindeadanalyzer/patterns");
+		TreeMap<String, Vulnerability> entryPoints = new TreeMap<String, Vulnerability>();
+		TreeMap<String, Vulnerability> sanitizationFunctions = new TreeMap<String, Vulnerability>();
+		TreeMap<String, Vulnerability> sensitiveSinks = new TreeMap<String, Vulnerability>();
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(patternsfile))) {
+		    String line;
+		    while ((line = br.readLine()) != null) {
+		       System.out.println(line);
+		    }
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
